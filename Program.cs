aaff8915a910b3fa4379a056a99a14dd1f2f9042
Vitoria <vitoriahellen3004@gmail.com>
2020-10-11 19:00:13 -0300

    Receba um número inteiro e exiba-o por extenso.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Humanizer; 
+
+namespace numeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Numero Extenso");
+            Console.Write("Digite um numero: "); 
+            numero =  Convert.ToInt32 (Console.ReadLine());
+            Console.Write($"{numero.ToWords()}");
+
+
+        }
+    }
+}
